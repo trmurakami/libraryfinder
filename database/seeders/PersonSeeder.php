f94dab0425fdef7b22049fb21c7b92376323a72c
Tiago Murakami <trmurakami@gmail.com>
2022-02-16 12:39:08 -0300

    person

---
+++ database/seeders/PersonSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Person;
+
+class PersonSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Person::factory()
+        ->count(50)
+        ->create();
+    }
+}
